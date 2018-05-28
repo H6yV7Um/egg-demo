d8bf2da6e50a01a96ec2a1becc30feaa3b9d0575
hardy <1111@qq.com>
2018-05-28 15:21:38 +0800

    '111'

--- config/config.default.js
+++ config/config.default.js
@@ -8,6 +8,25 @@ module.exports = appInfo => {
         hostname: '127.0.0.1',
         // path: '/var/run/egg.sock',
       }
+    },
+    mysql :{
+      // database configuration
+      client: {
+        // host
+        host: '10.105.92.125',
+        // port
+        port: '3306',
+        // username
+        user: 'yinzc',
+        // password
+        password: 'yin53719.',
+        // database
+        database: 'testdb1',    
+      },
+      // load into app, default is open
+      app: true,
+      // load into agent, default is close
+      agent: false,
     }
   };
   // use for cookie sign key, should change to your own and keep security
---
+++ libs/async-db.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql')
+const pool = mysql.createPool({
+  host     : '115.159.147.80',
+  user     : 'yinzc',
+  password : 'Yinzc53719.',
+  database : 'swtiny'
+});
+let query = function( sql, values ) {
+  return new Promise(( resolve, reject ) => {
+    pool.getConnection(function(err, connection) {
+      if (err) {
+        reject( err )
+      } else {
+        connection.query(sql, values, ( err, rows) => {
+
+          if ( err ) {
+            reject( err )
+          } else {
+            resolve( rows )
+          }
+          connection.release()
+        })
+      }
+    })
+  })
+};
+
+query.selectAllData = function (sql){
+  let dataList = query( sql )
+  return dataList
+};
+
+
+module.exports = query
